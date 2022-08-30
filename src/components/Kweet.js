a39ff540edc765153c46928b30fc869e60de9a2f
kimmy <hiyoung.kimmy@gmail.com>
2022-08-30 10:51:46 -0700

    5-4 Featured : Modify Tweet

--- src/components/Kweet.js
+++ src/components/Kweet.js
@@ -1,8 +1,14 @@
 import { dbService } from "fbase";
-import { deleteDoc, doc } from "@firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "@firebase/firestore";
+import { useState } from "react";
 
 const Kweet = ({ kweetObj, isOwner }) => {
     
+    const [editing, setEditing] = useState();
+    const [newKweet, setNewKweet] = useState(kweetObj.text);
+
+
+
     const onDeleteClick = async () => {
         const ok = window.confirm("Delete Tweet? Deleted messages cannot be recovered.");
         console.log(ok);
@@ -14,16 +20,44 @@ const Kweet = ({ kweetObj, isOwner }) => {
         }
     };
 
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onChange = (event) => {
+        const {
+            target : { value },
+        } = event;
+        setNewKweet(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        console.log(kweetObj.id, newKweet);
+        updateDoc(doc(dbService, "kweets", kweetObj.id), { text: newKweet });
+        setEditing(false);
+    };
 
     return (
         <div>
-            <h4>{kweetObj.text}</h4>
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button onClick={onDeleteClick}>Delete Kweet</button>
-                    <button>Edit Kweet</button>
+                    <form onSubmit={onSubmit}>
+                        <input onChange={onChange} value={newKweet} required />
+                        <input type="submit" value="Update Kweet" />
+                    </form>
+                    <button onClick={toggleEditing}> Cancel </button>
                 </>
-            )}      
+            ) : (
+                <>
+                        <h4>{kweetObj.text}</h4>
+                          {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>Delete Kweet</button>
+                            <button onClick={toggleEditing}>Edit Kweet</button>
+                         </>
+                    )}      
+                </>
+            )}
+
         </div>
     );
 };
